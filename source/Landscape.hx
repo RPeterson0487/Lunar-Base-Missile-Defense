0847f0d8713092232f995c408b1a65ba51bcc466
Richard Peterson <peterson4487@live.missouristate.edu>
2023-04-15 10:21:20 -0500

    Changed dimensions and finsihed basic landscape.

--- source/Landscape.hx
+++ source/Landscape.hx
@@ -5,7 +5,7 @@ import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
 class Landscape extends FlxSprite {
-	public function new(landscapeHeight = 50) {
+	public function new(landscapeHeight = 75) {
 		super();
 
 		makeGraphic(FlxG.width, landscapeHeight);
