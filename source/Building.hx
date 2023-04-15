a5d1b8dbe1cbaaed01dcf15bc4beb0a852894e07
Richard Peterson <peterson4487@live.missouristate.edu>
2023-04-15 14:16:03 -0500

    Added building placement and test kill functionality.

--- source/Building.hx
+++ source/Building.hx
@@ -1,9 +1,22 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 
-class Building extends FlxSprite{
-    public function new() {
-        super();
-    }
+class Building extends FlxSprite {
+	public function new() {
+		super();
+	}
+
+	public function destroyBuilding() {
+		this.kill();
+	}
+
+	override function update(elapsed:Float) {
+		if (FlxG.mouse.justPressedMiddle && FlxG.mouse.overlaps(this)) {
+			kill();
+		}
+
+		super.update(elapsed);
+	}
 }
